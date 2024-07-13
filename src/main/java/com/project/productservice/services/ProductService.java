e0e2c545cf67ea3d1eb6eebcd729614f7c172f62
Anup Deshmukh <anupdeshmukh96@gmail.com>
2024-07-13 22:17:26 +0530

    Implemented PUT, PATCH and DELETE methods

--- src/main/java/com/project/productservice/services/ProductService.java
+++ src/main/java/com/project/productservice/services/ProductService.java
@@ -1,11 +1,18 @@
 package com.project.productservice.services;
 
 import com.project.productservice.models.Product;
+
 import java.util.List;
 
 public interface ProductService {
 
-    Product getProductById(long id);
+    Product getProductById(long productId);
 
     List<Product> getAllProducts();
+
+    Product updateProduct(long productId, Product product);
+
+    Product replaceProduct(long productId, Product product);
+
+    Product deleteProduct(long productId);
 }
