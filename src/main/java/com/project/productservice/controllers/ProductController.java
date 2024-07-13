b4ab99ce2561354310919fe775f977326932ab8c
Anup Deshmukh <anupdeshmukh96@gmail.com>
2024-07-13 22:45:23 +0530

    Added Generic Exception Handler

--- src/main/java/com/project/productservice/controllers/ProductController.java
+++ src/main/java/com/project/productservice/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.project.productservice.controllers;
 import com.project.productservice.exceptions.ProductNotFoundException;
 import com.project.productservice.models.Product;
 import com.project.productservice.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +14,8 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
     private ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
 
     @GetMapping()
     public List<Product> getAllProducts() {
@@ -42,6 +40,11 @@ public class ProductController {
     @PutMapping("/{id}")
     public Product replaceProduct(@PathVariable("id") long id, @RequestBody Product product) {
         return productService.replaceProduct(id, product);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<?> handleException(Exception exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
