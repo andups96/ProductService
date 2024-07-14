de9648b709f0269f67222877276fa02ef563654c
Anup Deshmukh <anupdeshmukh96@gmail.com>
2024-07-14 07:33:29 +0530

    Implementation of Mapped Super Class DB Inheritance

--- src/main/java/com/project/productservice/models/Category.java
+++ src/main/java/com/project/productservice/models/Category.java
@@ -1,10 +1,12 @@
 package com.project.productservice.models;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category extends BaseModel {
     private long id;
     private String name;
