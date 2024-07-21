f5b5cec5db56ad62fd38e84c7e9247807ad7676c
Anup Deshmukh <anupdeshmukh96@gmail.com>
2024-07-21 18:27:10 +0530

    Projections, SQL & HQL Query Implementation

--- src/main/java/com/project/productservice/repositories/ProductRepository.java
+++ src/main/java/com/project/productservice/repositories/ProductRepository.java
@@ -1,14 +1,25 @@
 package com.project.productservice.repositories;
 
 import com.project.productservice.models.Product;
+import com.project.productservice.projections.ProductWithTitleAndId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findById(Long id);
+
+    //HQL Query
+    @Query("select p.id as id, p.title as title from Product p where p.id = :id")
+    List<ProductWithTitleAndId> findProductTitleById(Long id);
+
+    //SQL Query
+    @Query(value = "select * from product where id = :id", nativeQuery = true)
+    List<Product> findProductTitleByIdNative(Long id);
 
 }
